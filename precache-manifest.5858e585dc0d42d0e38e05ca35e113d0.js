5b4ed948be83e733dff3dbba985e118e4287865f
utkarshgupta2504 <utkarshgupta2504@gmail.com>
2020-07-20 22:12:32 +0530

    Updates

---
+++ precache-manifest.5858e585dc0d42d0e38e05ca35e113d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf8656e1a7e2c2a8a0d921e495eec94",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "58c10dfa6ad3d2dce1e9",
+    "url": "/keeper-app/static/js/2.ae13a3d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper-app/static/js/2.ae13a3d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13fdae1113a67db01c7f",
+    "url": "/keeper-app/static/js/main.ccb59ae6.chunk.js"
+  },
+  {
+    "revision": "3f7ec364bc84954cf321",
+    "url": "/keeper-app/static/js/runtime-main.afaa4161.js"
+  }
+]);
